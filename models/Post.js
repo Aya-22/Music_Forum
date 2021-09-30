d9d3e650beaf7b9805016f0f0cffbdec279c3614
Jennifer Ayala <j.ayala0695@gmail.com>
2021-09-29 20:15:24 -0700

    added column to post table

--- models/Post.js
+++ models/Post.js
@@ -40,6 +40,11 @@ Post.init(
         key: 'id',
         unique: false
       }
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
     }
   },
   {
