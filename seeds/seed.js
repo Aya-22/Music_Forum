4edf66209cbe9705575aae4bebf6857737685c13
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-09-29 21:07:34 -0700

    seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,27 +1,21 @@
 const sequelize = require('../config/connection');
-const { Band, Users, Concert, Post } = require('../models');
+const { Users, Band } = require('../models');
 
-const userData = require('./userData.json');
 const bandData = require('./bandData.json');
-const concertData = require('./concertData.json');
-const postData = require('./postData.json');
+const userData = require('./userData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const band = await Band.bulkCreate(bandData);
-
-  const concert = await Concert.bulkCreate(concertData);
-
-  const user = await Users.bulkCreate(userData, {
+  const users = await Users.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  for (const post of postData) {
-    await Post.create({
-      ...post,
-      user_id: user[Math.floor(Math.random() * user.length)].id,
+  for (const band of bandData) {
+    await Band.create({
+      ...Band,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
 
